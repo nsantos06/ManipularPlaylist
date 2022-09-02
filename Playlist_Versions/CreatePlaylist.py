dca14bfb1448971233e37f0a7648631957e83330
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-09-02 20:52:08 -0300

    Update and rename Adicionando_a_ultima_playlistCriada.py to CreatePlaylist.py

--- Playlist_Versions/CreatePlaylist.py
+++ Playlist_Versions/CreatePlaylist.py
@@ -22,10 +22,27 @@ token = SpotifyOAuth(client_id=CLIENT_ID,
                      scope=scope,
                      username=user_id)
 
-# Nome e descrição da playlist. Após, ela é criada.
-playlist = input("Name:")
-description = input("Description:")
-sp.user_playlist_create(user=user_id, name=playlist, description=description)
+def criar_Playlist():
+    # Nome e descrição da playlist. Após, ela é criada.
+    playlist = input("Name:")
+    description = input("Description:")
+    sp.user_playlist_create(user=user_id, name=playlist, description=description)
+
+def adicionando_playlist():
+    # Pegando as Informações da playlist do usuário.
+    playlist_user = sp.user_playlists(user=user_id)
+    # Pegando o ID da última playlist. Ou seja, a playlist que foi criada.
+    playlist_add = playlist_user['items'][0]['id']
+    # Adicionando os itens dentro da lista:
+    sp.playlist_add_items(playlist_id=playlist_add, items=list_of_tracks)
+
+def mostrar_playlist():
+    playlists = sp.user_playlists(user_id)
+    print("\nPlaylists:")
+    for playlist in playlists['items']:
+        print(playlist['name'])
+
+criar_Playlist()
 
 # Nome do Artista e da música
 artist = input("\nArtist:")
@@ -34,10 +51,10 @@ track = input("Track:")
 list_of_tracks = []
 contador_de_musicas = 0
 
-while track != '0':
-    #Só podemos adicionar um limite de 100 músicas na playlist.
+while contador_de_musicas != '0':
+    #Só podemos adicionar um limite de 100 músicas na playlist
     contador_de_musicas = contador_de_musicas + 1
-    
+
     # Fazendo a Busca do artista e da música.
     searchQuery = artist + ' ' + track
     searchResults = sp.search(q=searchQuery)
@@ -59,23 +76,12 @@ while track != '0':
     # Imprimindo a música adicionada e o cantor.
     print(f'\nMúsica {searchNameSong} do(a) {searchNameArtist} Adicionada!')
     print(f'Quantidade de músicas adicionadas:{contador_de_musicas}')
-    
-    if contador_de_musicas == 99:
+
+    if contador_de_musicas == 2:
         break
-        
+
     artist = input("\nArtist:")
     track = input("Track:")
 
-
-
-
-def playlist():
-    # Pegando as Informações da playlist do usuário.
-    playlist_user = sp.user_playlists(user=user_id)
-    # Pegando o ID da última playlist. Ou seja, a playlist que foi criada.
-    playlist_add = playlist_user['items'][0]['id']
-    # Adicionando os itens dentro da lista:
-    sp.playlist_add_items(playlist_id=playlist_add, items=list_of_tracks)
-
-playlist()
-
+adicionando_playlist()
+mostrar_playlist()
