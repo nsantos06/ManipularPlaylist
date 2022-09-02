0ad7ff7610831ec02a31dc5b37afc01da995d242
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-09-02 19:14:22 -0300

    Update Playlist4.0.py

--- Playlist_Versions/Playlist4.0.py
+++ Playlist_Versions/Playlist4.0.py
@@ -7,34 +7,38 @@ CLIENT_ID = ''
 CLIENT_SECRET = ''
 REDIRECT_URI = ''
 
-#Criando Objeto Spotify(nome sp).
+# Criando Objeto Spotify(nome sp).
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                scope=scope,
                                                redirect_uri=REDIRECT_URI))
-#Pegando o ID do usuário
+# Pegando o ID do usuário
 user_id = sp.me()['id']
 
-#Criando Token de Acesso à conta.
+# Criando Token de Acesso à conta.
 token = SpotifyOAuth(client_id=CLIENT_ID,
                      client_secret=CLIENT_SECRET,
                      redirect_uri=REDIRECT_URI,
                      scope=scope,
                      username=user_id)
 
-#Nome e descrição da playlist. Após, ela é criada.
+# Nome e descrição da playlist. Após, ela é criada.
 playlist = input("Name:")
 description = input("Description:")
 sp.user_playlist_create(user=user_id, name=playlist, description=description)
 
-#Nome do Artista e da música
+# Nome do Artista e da música
 artist = input("\nArtist:")
 track = input("Track:")
-#Lista onde as músicas seram guardadas.
+# Lista onde as músicas seram guardadas.
 list_of_tracks = []
+contador_de_musicas = 0
 
 while track != '0':
-    #Fazendo a Busca do artista e da música.
+    #Só podemos adicionar um limite de 100 músicas na playlist.
+    contador_de_musicas = contador_de_musicas + 1
+    
+    # Fazendo a Busca do artista e da música.
     searchQuery = artist + ' ' + track
     searchResults = sp.search(q=searchQuery)
 
@@ -47,22 +51,26 @@ while track != '0':
     searchNameSong = searchResults["tracks"]["items"][0]["name"]
     searchURISong = searchResults["tracks"]["items"][0]["uri"]
 
-    #print(f'\nArtista:{searchNameArtist}\nURI Artista:{searchURIArtist}')
-    #print(f'Nome da música:{searchNameSong}\nURI da Musica:{searchURISong}')
-    
-    #Adicionando a Música na lista.
+    # print(f'\nArtista:{searchNameArtist}\nURI Artista:{searchURIArtist}')
+    # print(f'Nome da música:{searchNameSong}\nURI da Musica:{searchURISong}')
+
+    # Adicionando a Música na lista.
     list_of_tracks.append(searchURISong)
-    #Imprimindo a música adicionada e o cantor.
+    # Imprimindo a música adicionada e o cantor.
     print(f'\nMúsica {searchNameSong} do(a) {searchNameArtist} Adicionada!')
+    print(f'Quantidade de músicas adicionadas:{contador_de_musicas}')
 
     artist = input("\nArtist:")
     track = input("Track:")
 
-#Pegando as Informações da playlist do usuário.
+
+
+
+# Pegando as Informações da playlist do usuário.
 playlist_user = sp.user_playlists(user=user_id)
 
-#Pegando o ID da última playlist. Ou seja, a playlist que foi criada. 
+# Pegando o ID da última playlist. Ou seja, a playlist que foi criada.
 playlist_add = playlist_user['items'][0]['id']
 
-#Adicionando os itens dentro da lista:
+# Adicionando os itens dentro da lista:
 sp.playlist_add_items(playlist_id=playlist_add, items=list_of_tracks)
